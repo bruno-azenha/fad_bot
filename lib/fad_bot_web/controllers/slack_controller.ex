15d4a22d33e6dc1841afe732aac463987586c9e2
Bruno Azenha Goncalves <azenha.bruno@gmail.com>
2021-03-07 21:56:02 +0200

    Change slash command ephemeral response
    Signed-off-by: Bruno Azenha Goncalves <azenha.bruno@gmail.com>

--- lib/fad_bot_web/controllers/slack_controller.ex
+++ lib/fad_bot_web/controllers/slack_controller.ex
@@ -4,18 +4,12 @@ defmodule FadBotWeb.SlackController do
   require Logger
 
   def callback(conn, %{"text" => text} = params) do
-    message = """
-    Slack calback received:
-
-        #{inspect(params)}
-    """
-
-    Logger.info(message)
-
     FadBot.Slack.post_message(text)
 
     conn
     |> put_status(:ok)
-    |> json("Ok, sending your message")
+    |> json("Your appreciation is on its way to <##{target_channel}>")
   end
+
+  defp target_channel(), do: Application.get_env(:fad_bot, :slack)[:target_channel_id]
 end
