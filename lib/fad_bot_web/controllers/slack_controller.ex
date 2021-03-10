29df3f9357e2f6b66f381ef8418cde420351830c
Bruno Azenha Goncalves <azenha.bruno@gmail.com>
2021-03-10 19:24:42 +0200

    Add Logging

--- lib/fad_bot_web/controllers/slack_controller.ex
+++ lib/fad_bot_web/controllers/slack_controller.ex
@@ -3,25 +3,33 @@ defmodule FadBotWeb.SlackController do
 
   require Logger
 
-  def callback(conn, %{"text" => text, "command" => command, "user_id" => user}) do
+  def callback(conn, %{
+        "text" => text,
+        "command" => command,
+        "user_id" => user_id,
+        "user_name" => user_name
+      }) do
+    log_message = "[FAD Log] sender: #{user_name}, message: #{text}"
+    Logger.info(log_message)
+
     message =
       case command do
-        "fad" -> build_message(text, user)
-        "fad-anon" -> build_anonymous_message(text)
+        "/fad" -> build_message(text, user_id)
+        "/fad-anon" -> build_anonymous_message(text)
       end
 
     FadBot.Slack.post_message(message)
 
     conn
     |> put_status(:ok)
-    |> json("Your appreciation is on its way to <##{target_channel}>")
+    |> json("Your appreciation is on its way to <##{target_channel()}>")
   end
 
-  defp build_message(text, user) do
+  defp build_message(text, user_id) do
     """
     #{text}
     ----
-    From <@#{user}>
+    From <@#{user_id}>
     """
   end
 
