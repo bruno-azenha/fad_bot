58cca5d74e73b1397a5a2ba6838d3e730240f6c3
Bruno Azenha Goncalves <azenha.bruno@gmail.com>
2021-03-10 18:51:06 +0200

    Add support for anonymous message
    Signed-off-by: Bruno Azenha Goncalves <azenha.bruno@gmail.com>

--- lib/fad_bot_web/controllers/slack_controller.ex
+++ lib/fad_bot_web/controllers/slack_controller.ex
@@ -3,13 +3,29 @@ defmodule FadBotWeb.SlackController do
 
   require Logger
 
-  def callback(conn, %{"text" => text} = params) do
-    FadBot.Slack.post_message(text)
+  def callback(conn, %{"text" => text, "command" => command, "user_id" => user}) do
+    message =
+      case command do
+        "fad" -> build_message(text, user)
+        "fad-anon" -> build_anonymous_message(text)
+      end
+
+    FadBot.Slack.post_message(message)
 
     conn
     |> put_status(:ok)
     |> json("Your appreciation is on its way to <##{target_channel}>")
   end
+
+  defp build_message(text, user) do
+    """
+    #{text}
+    ----
+    From <@#{user}>
+    """
+  end
+
+  defp build_anonymous_message(text), do: text
 
   defp target_channel(), do: Application.get_env(:fad_bot, :slack)[:target_channel_id]
 end
