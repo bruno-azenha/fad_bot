4e9664873a991d30219c08aa02db25c393f00d80
Bruno Azenha Goncalves <azenha.bruno@gmail.com>
2023-01-31 07:12:32 -0300

    love bot again

--- lib/fad_bot_web/controllers/slack_controller.ex
+++ lib/fad_bot_web/controllers/slack_controller.ex
@@ -9,20 +9,19 @@ defmodule FadBotWeb.SlackController do
         "user_id" => user_id,
         "user_name" => user_name
       }) do
-    log_message = "[FAD Log] sender: #{user_name}, message: #{text}"
+    log_message = "[Love Log] sender: #{user_name}, message: #{text}"
     Logger.info(log_message)
 
     message =
       case command do
-        "/fad" -> build_message(text, user_id)
-        "/fad-anonymous" -> build_anonymous_message(text)
+        "/love" -> build_anonymous_message(text)
       end
 
     FadBot.Slack.post_message(message)
 
     conn
     |> put_status(:ok)
-    |> json("Your appreciation is on its way to <##{target_channel()}>")
+    |> json("Your love is on its way to <##{target_channel()}>")
   end
 
   defp build_message(text, user_id) do
